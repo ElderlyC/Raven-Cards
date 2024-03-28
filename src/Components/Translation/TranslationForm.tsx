5f3ff3bd7c8c7a7d6e78d6c79ccf8111b62c4883
Corey <corey260596@gmail.com>
2024-03-28 15:15:02 +1000

    testing japanese translation

--- src/Components/Translation/TranslationForm.tsx
+++ src/Components/Translation/TranslationForm.tsx
@@ -4,6 +4,8 @@ import { TextField, Button, Typography, Box } from "@mui/material";
 import axios from "axios";
 import classes from "./TranslationForm.module.css";
 
+// handle japanese translation
+
 type TranslationFormTypes = {
   onTranslation: ({ source, target }: WordPair) => void;
   smallScreen: boolean;
@@ -14,10 +16,10 @@ const TranslationForm: React.FC<TranslationFormTypes> = ({
   smallScreen,
 }) => {
   const [text, setText] = useState("");
-  const [langs, setLangs] = useState(["ko", "en"]);
+  const [langs, setLangs] = useState(["ko", "en", "ja"]);
   const [loading, setLoading] = useState(false);
 
-  const langNames = { ko: "Korean", en: "English" };
+  const langNames = { ko: "Korean", en: "English", ja: "Japanese" };
 
   const handleSubmit = async (e: FormEvent<HTMLFormElement>) => {
     e.preventDefault();
@@ -29,7 +31,8 @@ const TranslationForm: React.FC<TranslationFormTypes> = ({
         meaning: string;
       }>(
         "https://australia-southeast1-ko-en-cards.cloudfunctions.net/Ko-En-Cards",
-        { source: langs[0], target: langs[1], text: text }
+        // { source: langs[0], target: langs[1], text: text } // testing japanese
+        { source: "auto", target: "en", text: text } // testing japanese
       );
 
       onTranslation({ source: text, target: response.data.translation });
@@ -40,8 +43,12 @@ const TranslationForm: React.FC<TranslationFormTypes> = ({
     setLoading(false);
   };
 
+  const handleShuffle = () => {
+    setLangs(([lang1, lang2, lang3]) => [lang3, lang1, lang2]);
+  };
+
   const handleSwap = () => {
-    setLangs(([lang1, lang2]) => [lang2, lang1]);
+    setLangs(([lang1, lang2, lang3]) => [lang2, lang1, lang3]);
   };
 
   const handleTextChange = (e) => {
@@ -50,7 +57,7 @@ const TranslationForm: React.FC<TranslationFormTypes> = ({
       (/[a-zA-Z+]/.test(text.slice(-1)) && langs[0] === "ko") ||
       (koReg.test(text.slice(-1)) && langs[0] === "en")
     )
-      handleSwap();
+      handleSwap(); // this code is now broken
     setText(e.target.value);
   };
 
@@ -64,6 +71,17 @@ const TranslationForm: React.FC<TranslationFormTypes> = ({
             {"<  to  >"}
           </Button>
           {langNames[langs[1] as keyof typeof langNames]}
+        </Typography>
+        <Typography variant="h5" fontWeight={"bold"}>
+          <Button onClick={handleShuffle} size="large">
+            {langNames[langs[0] as keyof typeof langNames]}
+          </Button>
+          <Button onClick={handleSwap} size="large">
+            {"<  to  >"}
+          </Button>
+          <Button onClick={handleShuffle} size="large">
+            {langNames[langs[1] as keyof typeof langNames]}
+          </Button>
         </Typography>
       </Box>
 
